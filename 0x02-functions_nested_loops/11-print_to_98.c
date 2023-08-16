40f039eb43072156f8d4c8019d7a601b80acd02a
kelvin <kelvinokoye413@gmail.com>
2023-08-15 19:51:43 -0700

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 #include "main.h"
-/**
- * print_to_98 - function to print absolute values
- * @n: a variable to contain the number to be checked
- */
 
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
 void print_to_98(int n)
 {
-	int i;
+	int i, j;
 
-	for (i = n; i < 99; i++)
+	if (n <= 98)
 	{
-		printf("%d", i);
-		if (n == 98)
-			break;
-		printf(", ");
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
 	}
-	_putchar('\n');
 }
+
+
