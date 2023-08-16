bd6633d1bde4971811a6af1bad74679ffefc99cd
kelvin <kelvinokoye413@gmail.com>
2023-08-15 21:57:24 -0700

    done

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
-#include "main.h"
-/**
- * main - function to print absolute values
- *
- * Return: returns 0 for end of program
- */
 
+/**
+ * main - finds and prints the sum of the even-valued terms
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
-	int sum;
-	unsigned long long fibo[];
+	unsigned long int j, k, next, sum;
 
-	fibo[0] = 1;
-	fibo[1] = 2;
+	j = 1;
+	k = 2;
+	sum = 0;
 
-	for (i = 2; fibo[i] < 4000000; i++)
+	for (i = 1; i <= 33; ++i)
 	{
-		fibo[i] = fibo[i - 1] + fibo[i - 2];
-		if (fibo[i] % 2 == 0)
-			sum += fibo[i];
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
 	}
-	printf("%d\n", sum);
+
+	printf("%lu\n", sum);
+
 	return (0);
 }
+
