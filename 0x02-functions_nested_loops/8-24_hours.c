3733d04ced0c87df7a633bbb9b0d72e24b0cbbc0
kelvin <kelvinokoye413@gmail.com>
2023-08-15 17:50:49 -0700

    done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < 24)
+
