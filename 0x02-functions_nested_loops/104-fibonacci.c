9d86850bd0b1a8ea4d9bb90a3a2dc696905633e2
kelvin <kelvinokoye413@gmail.com>
2023-08-15 21:47:30 -0700

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
- * main - function to print absolute values
+ * main - fibonacci <3
  *
- * Return: returns 0 for end of program
+ * Purpose - no hardcode
+ *
+ * Return:  (Success)
  */
 
 int main(void)
 {
-	int i;
-	unsigned int fibo1 = 1;
-	unsigned int fibo2 = 2;
-	unsigned int fibo_next;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-	printf("%u, %u ", fibo1, fibo2);
-	for (i = 2; i < 98; i++)
+	printf("%lu", bef);
+
+	for (i = 1; i < 91; i++)
 	{
-		fibo_next = fibo1 + fibo2;
-		if (i == 97)
-		{
-			printf("%u", fibo_next)
-		}
-		else
-		{
-			printf("%u, ", fibo_next);
-		}
-		fibo1 = fibo2;
-		fibo2 = fibo_next;
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
+
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
 	return (0);
