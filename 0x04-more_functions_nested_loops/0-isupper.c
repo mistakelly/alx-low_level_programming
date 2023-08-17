c60514686933c9862c661d408b0cfedaed4ed0e3
kelvin <kelvinokoye413@gmail.com>
2023-08-17 05:13:40 -0700

    done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+*/
+
+int _isupper(int x)
+
+{
+
+	if (x >= 65 && x <= 90)
+	return (1);
+	return (0);
+}
+
+
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * multiplication - a function that multiplies two numbers
+ * @x,y numbers to be multiplied
+ * Return: x and y
+ */
+
+int mul(int x, int y)
+
+{
+	return (x * y);
+}
