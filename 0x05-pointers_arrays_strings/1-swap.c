ec721f51e3217eb46e25979c0aca769e06c83334
kelvin <kelvinokoye413@gmail.com>
2023-08-21 14:59:32 -0700

    done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap two integer - a function that swaps the values of two integers
+ * @n: input
+ * Return: *a = *b
+ */
+void swap_int(int *a, int *b);
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+
+}
