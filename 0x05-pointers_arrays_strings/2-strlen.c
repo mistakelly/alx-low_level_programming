0346e273667849993a05beb42579c983fa64e302
kelvin <kelvinokoye413@gmail.com>
2023-08-21 15:26:03 -0700

    done

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,24 +1,19 @@
 #include "main.h"
-
 /**
  * _strlen - returns the length of a string
  * @s: string
  * Return: length
  */
-
-int _strlen(char *s);
+int _strlen(char *s)
 {
-	int count = 0;
+	int longi = 0;
 
 	while (*s != '\0')
-
 	{
-		count++;
+		longi++;
 		s++;
-
 	}
 
-	return (count);
-
+	return (longi);
 }
 
