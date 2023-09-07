120abb8033f2e8cfd02b3ce40ce412912d93d453
kelvin <kelvinokoye413@gmail.com>
2023-09-06 17:37:52 -0700

    done

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,11 @@
 #include "main.h"
-void _puts(char *str);
 #include<stdio.h>
 /**
- * _puts - Prints a string to stdout.
- * @str: The string to be printed.
- *
- * Return: None.
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
-void _puts(char *str)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
+	return (0);
 }
