9c258bc26d2089f542292bb3e7c338e006df8b79
kelvin <kelvinokoye413@gmail.com>
2023-09-11 15:13:36 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the name of  the name of the program
+ *
+ * Return: - always 0 (sucess).
+ */
+int main(void)
+{
+	printf("%s\xA, __FILE__");
+	return (0);
+
+}
