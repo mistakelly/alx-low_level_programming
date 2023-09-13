9e6b036ce98549ab35b89b4ad56c014a081a5c99
kelvin <kelvinokoye413@gmail.com>
2023-09-12 17:00:04 -0700

    REVISION

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,14 +7,24 @@
  */
 void puts2(char *str)
 {
-	int length = 0;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-	while (str[length] != '\0')
+	while (*y != '\0')
 	{
-		length += 2;
-		_putchar(str[length]);
+		y++;
+		longi++;
 	}
-
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
 	_putchar('\n');
 }
 
