7d50707856ccfbab0eea8594751ab41f9c3b31a3
kelvin <kelvinokoye413@gmail.com>
2023-11-13 20:09:23 -0800

    creating python-shared library in c

---
+++ 0x18-dynamic_libraries/every_func.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+/**
+ * modulus - Computes the modulus of two integers
+ * @a: The dividend
+ * @b: The divisor
+ *
+ * Return: The remainder when a is divided by b
+ */
+int modulus(int a, int b)
+{
+	return (a % b);
+}
+
+
+/**
+ * divide - Divides two integers
+ * @a: The dividend
+ * @b: The divisor
+ *
+ * Return: The result of dividing a by b
+ */
+
+int divide(int a, int b)
+{
+
+	return (a / b);
+}
+
+/**
+ * subtract - Subtracts two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The result of subtracting the second integer from the first
+ */
+int subtract(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * add - Adds two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The sum of the two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * multiply - Multiplies two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The product of the two integers
+ */
+int multiply(int a, int b)
+{
+	return (a * b);
+}
